e1d6bef0fe54ab55de5ed4981babde4c118e8386
Hnii201 <hanin.qr20@gmail.com>
2022-10-17 01:33:21 +0300

    add sounds to player and doors

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -7,27 +7,27 @@ public class FPController : MonoBehaviour
     // Camera Variables
     [SerializeField] private Transform cameraTarget;
     private Camera mainCamera;
-  
-    [SerializeField] private bool invertMouse = false;
-   
+
+
     private float verticalRotationLimit;
 
     // Mouse Variables 
     [SerializeField] private float mouseSensitivity;
     [SerializeField] private float lookUpConstraint;
     [SerializeField] private float lookDownConstraint;
+    [SerializeField] private bool invertMouse = false;
 
     // Movement Variables 
-    //[SerializeField] private float WalkSpeed;
-    //[SerializeField] private float RunSpeed;
-    //private CharacterController charController;
-  //  private Vector3 movement;
+    [SerializeField] private float WalkSpeed;
+    [SerializeField] private float RunSpeed;
+    private CharacterController charController;
+    private Vector3 movement;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       // charController = GetComponent<CharacterController>();
+        charController = GetComponent<CharacterController>();
         mainCamera = Camera.main;
     }
 
@@ -35,33 +35,33 @@ public class FPController : MonoBehaviour
     private void Update()
     {
         // Mouse Input
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"))*mouseSensitivity ;
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
         //Rotation the player horizontally based on Mouse X Input 
         float yRotation = transform.rotation.eulerAngles.y + mouseInput.x;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, yRotation, transform.rotation.eulerAngles.z);
 
         // Mouse Invert Preference 
-       float invert = (!invertMouse) ? -1f : 1f;
-       verticalRotationLimit += (mouseInput.y * invert);
+        float invert = (!invertMouse) ? -1f : 1f;
+        verticalRotationLimit += (mouseInput.y * invert);
 
         // Limit the X axis rotation based on input
-        verticalRotationLimit = Mathf.Clamp(verticalRotationLimit, lookDownConstraint,lookUpConstraint );
+        verticalRotationLimit = Mathf.Clamp(verticalRotationLimit, lookDownConstraint, lookUpConstraint);
 
         //Applies rotation to the camera target
         cameraTarget.rotation = Quaternion.Euler(verticalRotationLimit, cameraTarget.eulerAngles.y, cameraTarget.eulerAngles.z);
 
         //User input for moving 
-        //Vector3 moveForward = transform.forward * Input.GetAxisRaw("Vertical");
-        //Vector3 moveRight = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 moveForward = transform.forward * Input.GetAxisRaw("Vertical");
+        Vector3 moveRight = transform.right * Input.GetAxisRaw("Horizontal");
 
         //set movement speed based on if the player is holding down left shift
-        //float currentSpeed = (Input.GetKey(KeyCode.LeftShift)) ? RunSpeed : WalkSpeed;
+        float currentSpeed = (Input.GetKey(KeyCode.LeftShift)) ? RunSpeed : WalkSpeed;
 
 
         // Generate movement 
-       // movement = (moveForward + moveRight).normalized * currentSpeed;
-        //charController.Move(movement * Time.deltaTime);
+        movement = (moveForward + moveRight).normalized * currentSpeed;
+        charController.Move(movement * Time.deltaTime);
 
     }
 
